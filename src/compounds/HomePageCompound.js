04d31f66fcee80472d6806688732af27236598a7
Jomana ahmed <ojy.ahmed@outlook.com>
2024-08-04 17:51:07 +0200

    Adding Banner.js file to the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/HomePage/Navbar";
+import Banner from "../components/HomePage/Banner";
 
 function HomePageCompound() {
   return (
     <Wrapper>
       <Navbar />
+      <Banner />
     </Wrapper>
   );
 }
