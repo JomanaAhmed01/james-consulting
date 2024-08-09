14eef3f11f3634a00fec14d184cae6783c76e069
Jomana ahmed <ojy.ahmed@outlook.com>
2024-08-09 17:15:58 +0200

    Adding Services.js file to the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/HomePage/Navbar";
 import Banner from "../components/HomePage/Banner";
+import Services from "../components/HomePage/Services";
 
 function HomePageCompound() {
   return (
     <Wrapper>
       <Navbar />
       <Banner />
+      <Services />
     </Wrapper>
   );
 }
