81aff3e2b10c8d55e3361777f78078cf955c52f8
Jomana ahmed <ojy.ahmed@outlook.com>
2024-08-11 23:56:48 +0200

    Creating OurClients.js file

---
+++ src/components/HomePage/OurClients.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function OurClients() {
+  return <Wrapper></Wrapper>;
+}
+
+export const Wrapper = styled.div``;
+
+export default OurClients;
