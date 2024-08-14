2505510370d1cfa48d0b8c08c00dd6ef1b8d2318
Jomana ahmed <ojy.ahmed@outlook.com>
2024-08-14 10:59:51 +0200

    Adding Testimonials.js file to the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -5,6 +5,7 @@ import Banner from "../components/HomePage/Banner";
 import Services from "../components/HomePage/Services";
 import AboutUs from "../components/HomePage/AboutUs";
 import OurClients from "../components/HomePage/OurClients";
+import Testimonials from "../components/HomePage/Testimonials";
 
 function HomePageCompound() {
   return (
@@ -14,6 +15,7 @@ function HomePageCompound() {
       <Services />
       <AboutUs />
       <OurClients />
+      <Testimonials />
     </Wrapper>
   );
 }
