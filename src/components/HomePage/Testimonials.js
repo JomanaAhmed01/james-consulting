3fd382fb5a7cb1a1069b458fbbf1c218d940147e
Jomana ahmed <ojy.ahmed@outlook.com>
2024-08-14 10:59:01 +0200

    Creating Testimonials.js file

---
+++ src/components/HomePage/Testimonials.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Testimonials() {
+  return <Wrapper></Wrapper>;
+}
+
+export const Wrapper = styled.div``;
+
+export default Testimonials;
